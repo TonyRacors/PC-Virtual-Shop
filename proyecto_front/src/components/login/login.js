a604ecfb27198ffcee115bc095f9c54d5b22a80a
Yeisondqv <yeison.qv97@gmail.com>
2022-11-01 01:50:55 -0500

    Ajustes al login y creacion navbar

---
+++ proyecto_front/src/components/login/login.js
@@ -0,0 +1,59 @@
+import React from "react";
+
+import { Container, Form, Button, Row, Col , Dropdown} from "react-bootstrap";
+import "./login.css";
+
+export default class login extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  
+  state = {};
+  render() {
+
+    return (
+
+      <Container id="login-container">
+        <Row>
+          <Col sm="12" xs="12" md={{ span: 4, offset: 4 }} lg={{ span: 4, offset: 4 }} xl={{ span: 4, offset: 4 }}>
+           
+            <Row>
+              <h2>¡Bienvenido!</h2>
+            </Row>
+
+            <Row>
+              <Form>
+
+                <Form.Group className="mb-3" controlId="formBasicUser" >
+                  <Form.Label>Usuario</Form.Label>
+                  <Form.Control
+                    onChange={(e) => this.setState({ usuario: e.target.value })}
+                    placeholder="Por favor ingresa tu usuario."
+                  />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                  <Form.Label>Contraseña</Form.Label>
+                  <Form.Control
+                    type="password"
+                    placeholder="Por favor ingresa tu contraseña."
+                    onChange={(e) => this.setState({ pass: e.target.value })}
+                  />
+                </Form.Group>
+
+                <Button variant="primary" type="submit">
+                  Iniciar sesión
+                </Button>
+
+                <Dropdown.Divider/>
+
+                <a href="register">¿Aún no posees una cuenta?</a>
+
+              </Form>
+            </Row>
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
+}
